3b441c5d1ba68d209905ef0d4affb0e760dbab5c
caimenjackson <73300686+caimenjackson@users.noreply.github.com>
2022-03-21 10:24:20 +0000

    Add files via upload

---
+++ src/transaction_handler.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author caimenjackson
+ *
+ */
+public class transaction_handler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int customer_balance_checker(int CustomerNo) {
+		return null;
+	}
+	
+	
+	
+	
+	public static String withdraw_handler(int CustomerNo, int WithdrawAmount) {
+		
+		return null;
+		
+	}
+
+}
